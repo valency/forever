97e2baa381d1f06e92f511e20fbead53c522371b
Ye Ding <yding@deepera.com>
2018-04-09 14:56:17 +0800

    v0.4.9

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name='forever',
     version='0.4.5',
     packages=['.'],
-    url='http://open.deepera.com',
+    url='https://github.com/valency/forever',
+    download_url = 'https://github.com/valency/forever/releases/download/v0.4.5/forever-0.4.5.tar.gz',
     license='CPL-3.0',
     author='Deepera Co., Ltd.',
     author_email='yding@deepera.com',
-    description='Forever',
+    description='Forever: auto restart any script when it stops printing.',
+    keywords = ['forever', 'logging', 'auto-restart'],
     install_requires=[]
 )
